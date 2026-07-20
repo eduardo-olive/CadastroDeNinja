8733ad496dba538ae45cd5a8686405a7ff700776
eduardo-olive <oliveira.eduardo.dev@gmail.com>
2026-07-20 20:30:50 -0300

    feat: adicionado interface de repository para missoes.

---
+++ src/main/java/dev/java10x/CadastroNinjas/MIssoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroNinjas.MIssoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissaoModel, Long> {
+}

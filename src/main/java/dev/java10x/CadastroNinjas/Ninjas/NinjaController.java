254b9061d05bdd3d3db04665ae59743036381001
eduardo-olive <oliveira.eduardo.dev@gmail.com>
2026-07-22 19:34:01 -0300

    feat: Adicionado as rotas de create, read, update e delete no NinjaController

--- src/main/java/dev/java10x/CadastroNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroNinjas/Ninjas/NinjaController.java
@@ -1,17 +1,41 @@
 package dev.java10x.CadastroNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class NinjaController {
 
-
     @GetMapping("/wellcome")
     public String boasVindas(){
         return "Essa é a minha primeira mensagem nessa rota.";
     }
+
+    // Adcionar ninja (CREATE)
+    @PostMapping("/nija")
+    public String criarNinja() {
+        return "Ninja criado.";
+    }
+
+    // Mostar todos os ninjas (READ)
+    @GetMapping("/ninja")
+    public String mostrarNinjas(){
+        return "Mostra todos os ninjas cadastrados.";
+    }
+
+    // Mostar ninja por ID (READ)
+    @GetMapping("/ninja/:id")
+    public String mostarNinja(){
+        return "Mostra ninja por ID.";
+    }
+
+    // Alterar dados do ninja (UPDATE)
+    @PutMapping("/nija/:id")
+    public String atualizarNinja(){
+        return "Ninja atualizado";
+    }
+
+
+    // Deletar ninja (DELETE)
 
 }

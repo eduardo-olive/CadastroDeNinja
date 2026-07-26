9723c78ec27737af856aca6072583ae07c5aaf52
eduardo-olive <oliveira.eduardo.dev@gmail.com>
2026-07-26 00:59:18 -0300

    feat: Inclui DTO como abstação para a entidade na camada de service e controller.

--- src/main/java/dev/java10x/CadastroNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroNinjas/Ninjas/NinjaController.java
@@ -21,7 +21,7 @@ public class NinjaController {
 
     // Adcionar ninja (CREATE)
     @PostMapping("/ninja")
-    public NinjaModel criarNinja(@RequestBody NinjaModel ninja) {
+    public NinjaDTO criarNinja(@RequestBody NinjaDTO ninja) {
         return ninjaService.criarNinja(ninja);
     }
 
